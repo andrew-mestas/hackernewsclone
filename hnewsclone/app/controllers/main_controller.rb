d8d4a0e73b00543fa7999121e3ad27a124fd4efe
Andrew Mestas <andrew.mestas.dev@gmail.com>
2015-11-16 16:57:22 -0800

    user authentication

---
+++ hnewsclone/app/controllers/main_controller.rb
@@ -0,0 +1,8 @@
+class MainController < ApplicationController
+	  before_action :is_authenticated?, except: [:index]
+
+  def index
+  end
+  
+end
+
---
+++ hnewsclone/app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		user = User.authenticate login_params["email"], login_params["password"]
+		if user 
+		  session[:user_id] = user.id
+		  flash[:success] = "#{user.email} has logged in"
+		  redirect_to login_path
+		else
+		  flash[:danger] = "You had the wrong email/password"
+		  redirect_to login_path
+		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		redirect_to root_path
+	end
+
+	private
+	  def login_params
+	  	params.require(:user).permit(:email, :password)
+	  end
+end
